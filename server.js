40d06ef45a0273ea22c55deae24f33009c951b7b
Nompilon <ngwenyanompilo@gmail.com>
2026-01-19 00:21:59 +0200

    learning activies wk3 done and working

--- server.js
+++ server.js
@@ -5,11 +5,14 @@
 /* ***********************
  * Require Statements
  *************************/
+const baseController = require("./controllers/baseController")
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")
 
 
 /* ***********************
@@ -23,12 +26,36 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-
 /* ***********************
  * Index route
  *************************/
-app.get("/", function (req, res){
-  res.render("index",{title: "Home"})
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+/* ***********************
+*Inventory routes
+*************************/
+app.use("/inv", inventoryRoute)
+
+
+/* ***********************
+ * 404 handler
+ *************************/
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." })
+})
+
+/* ***********************
+ * Express error handler
+ *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
 })
 
 /* ***********************
